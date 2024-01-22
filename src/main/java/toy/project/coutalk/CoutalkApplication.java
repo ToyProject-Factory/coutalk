2cf05bf8fbd978a9234c5a07bef44994dc0308bc
JaeYoung Park <jyoung_9154@naver.com>
2024-01-22 00:25:40 -0500

    [생성] Toy Project : Coutalk (쿠톡)

---
+++ src/main/java/toy/project/coutalk/CoutalkApplication.java
@@ -0,0 +1,15 @@
+package toy.project.coutalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoutalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoutalkApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/toy/project/coutalk/CoutalkApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.project.coutalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoutalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
