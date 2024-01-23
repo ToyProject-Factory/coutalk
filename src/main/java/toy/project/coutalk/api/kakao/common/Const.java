e332b44b6b1cc8a1fc7a4cf82a2bf2144bc132a8
JaeYoung Park <jyoung_9154@naver.com>
2024-01-23 18:56:58 -0500

    [생성] 카카오 API 연동 1. 카카오톡 API 연동 및 테스트용 화면단 추가 2. HttpSession 등록 시 tomcat.jar가 문제가 있어서 직접 다운받아서 추가 3. 프로퍼티 파일에 App-Key 등록

---
+++ src/main/java/toy/project/coutalk/api/kakao/common/Const.java
@@ -0,0 +1,8 @@
+package toy.project.coutalk.api.kakao.common;
+
+public class Const {
+    public static final String POST = "POST";
+    public static final String GET = "GET";
+
+    public static final String EMPTY = "";
+}
