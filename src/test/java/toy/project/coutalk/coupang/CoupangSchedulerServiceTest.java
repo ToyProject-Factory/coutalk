740075c6da511a62629eb485c3ae1b96f48643f9
nuclearmonkey21 <nuclearmonkey21@icloud.com>
2024-02-15 22:57:18 +0900

    [test] 스케줄서비스 테스트 코드작성

--- src/test/java/toy/project/coutalk/coupang/CoupangSchedulerServiceTest.java
+++ src/test/java/toy/project/coutalk/coupang/CoupangSchedulerServiceTest.java
@@ -3,8 +3,14 @@ package toy.project.coutalk.coupang;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import toy.project.coutalk.coupang.service.CoupangSchedulerService;
 
-public class SchedulerTest {
+@SpringBootTest
+public class CoupangSchedulerServiceTest {
+    @Autowired
+    CoupangSchedulerService coupangSchedulerService;
     @BeforeEach
     void setupTest() {
 
@@ -14,5 +20,7 @@ public class SchedulerTest {
     void tearDown() { }
 
     @Test
-    void test() {}
+    void test() {
+        while(true);
+    }
 }
