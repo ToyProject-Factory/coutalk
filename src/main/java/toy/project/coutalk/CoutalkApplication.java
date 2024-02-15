9bc540c5aba0330cbd51d3c2865e6256a63d021d
nuclearmonkey21 <nuclearmonkey21@icloud.com>
2024-02-15 22:56:41 +0900

    [feat] 스케줄링 서비스 생성

--- src/main/java/toy/project/coutalk/CoutalkApplication.java
+++ src/main/java/toy/project/coutalk/CoutalkApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import toy.project.coutalk.controller.CoutalkController;
 import toy.project.coutalk.jpa.repository.CoupangProductInfoRepository;
 
 @EntityScan(basePackages = {"toy.project.coutalk.jpa.domain", "toy.project.coutalk.coupang.domain"})
 @SpringBootApplication
+@EnableScheduling
 public class CoutalkApplication {
 
     public static void main(String[] args) {
--- src/main/java/toy/project/coutalk/coupang/service/CoupangSchedulerService.java
+++ src/main/java/toy/project/coutalk/coupang/service/CoupangSchedulerService.java
@@ -1,5 +1,6 @@
 package toy.project.coutalk.coupang.service;
 
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -8,22 +9,41 @@ import toy.project.coutalk.coupang.repository.CoupangProductRepository;
 
 import java.util.List;
 
+/**
+ *  쿠팡 제품 정보 스케줄 서비스.
+ *
+ *  <p>
+ *      coupangProductService를 이용하여 시간마다 데이터 베이스에 저장된 제품의 가격을 현재 사이트에 올려지 가격과 비교
+ *  </p>
+ *
+ * @version 0.0.1
+ * @author nuclearmonkey21
+ */
 @Service
-public class CoupangProductSchedulerService {
+public class CoupangSchedulerService {
     private final CoupangProductRepository coupangProductRepository;
     private final CoupangProductService coupangProductService;
     @Autowired
-    public CoupangProductSchedulerService(CoupangProductRepository coupangProductRepository, CoupangProductService coupangProductService) {
+    public CoupangSchedulerService(CoupangProductRepository coupangProductRepository, CoupangProductService coupangProductService) {
         this.coupangProductRepository = coupangProductRepository;
         this.coupangProductService = coupangProductService;
     }
-    @Scheduled(cron = "0/10 * * * * *") // Execute every hour
-    public void searchAndUpdateProducts() {
-        List<CoupangProduct> items = coupangProductRepository.findAll();
 
-        for (CoupangProduct item : items) {
-            // Assuming you have getters for itemId and productId in CoupangItemEntity
-            System.out.println("@Scheduled annotation : 10초에 1번씩 console 찍기");
+    /**
+     *  스케줄 메소드.
+     *
+     *  <p>
+     *      데이터 베이스에 저장된 제품의 정보 업데이트
+     *  </p>
+     *
+     */
+    //todo Test 끝나고 1시간 단위로 바꿀예정
+    @Scheduled(cron = "0 0 * * * *")
+    //@Scheduled(cron = "0 0 * * * *")
+    public void searchAndUpdateProducts() {
+        List<CoupangProduct> products = coupangProductRepository.findAll();
+        for (CoupangProduct product : products) {
+            coupangProductService.getProductUrl(product.getItemId(),product.getProductId());
         }
     }
 }
